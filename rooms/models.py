5df94c8d67b930142ce690258c747f155aabac75
LeeSoMyoung <so4644009@gmail.com>
2021-03-09 21:55:23 +0900

    ~#5.0 Review Model

--- rooms/models.py
+++ rooms/models.py
@@ -18,6 +18,42 @@ class AbstractItem(core_models.TimeStampedModel):
 
 class RoomType(AbstractItem):
     # 데이터베이스에 다른 타입이 들어감
+    """Room Type Object Definition"""
+
+    class Meta:
+        verbose_name="Roome Type"
+       # ordering=["-created"]
+    pass
+
+class Amenity(AbstractItem):
+    """Amenity Object Definition"""
+
+    class Meta:
+        verbose_name_plural="Amenities"
+    pass
+
+class Facility(AbstractItem):
+    """Facility Model Definiton"""
+
+    class Meta:
+            verbose_name_plural="Facilities"
+    pass
+
+class HouseRule(AbstractItem):
+    """House Rule Model Definition"""
+
+    class Meta:
+        verbose_name="House Rule"
+    pass
+
+class Photo(core_models.TimeStampedModel):
+    """Photo Model Definition"""
+    caption = models.CharField(max_length=80)
+    file=models.ImageField()
+    room=models.ForeignKey("Room",on_delete=models.CASCADE)
+    def __str__(self):
+        return self.caption
+
     pass
 
 class Room(core_models.TimeStampedModel):
@@ -38,7 +74,10 @@ class Room(core_models.TimeStampedModel):
     check_out=models.TimeField()
     instant_book=models.BooleanField(default=False)
     host=models.ForeignKey(user_models.User, on_delete=models.CASCADE) # users와 연결
-    room_type=models.ManyToManyField(RoomType, blank=True)
+    room_type=models.ForeignKey(RoomType, on_delete=models.SET_NULL,null=True)
+    amenities=models.ManyToManyField(Amenity,blank=True)
+    facilities=models.ManyToManyField(Facility, blank=True)
+    rules=models.ManyToManyField(HouseRule,blank=True)
 
     def __str__(self):
         return self.name
